f2d31128077f4c62acda7be4fd19315521bdb39f
Esteve <contacto@emestre.es>
2018-11-06 19:34:54 +0100

    Bot 17:27

--- GEOPADEL/DB/db_geopadel.sql
+++ GEOPADEL/DB/db_geopadel.sql
@@ -3,7 +3,7 @@
 -- https://www.phpmyadmin.net/
 --
 -- Servidor: 127.0.0.1
--- Tiempo de generación: 02-11-2018 a las 17:31:27
+-- Tiempo de generación: 06-11-2018 a las 19:34:25
 -- Versión del servidor: 10.1.26-MariaDB
 -- Versión de PHP: 7.1.8
 
@@ -25,32 +25,60 @@ SET time_zone = "+00:00";
 -- --------------------------------------------------------
 
 --
+-- Estructura de tabla para la tabla `levels`
+--
+
+CREATE TABLE `levels` (
+  `levels_id` int(5) NOT NULL,
+  `levels_desc` varchar(25) NOT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=latin1;
+
+--
+-- Volcado de datos para la tabla `levels`
+--
+
+INSERT INTO `levels` (`levels_id`, `levels_desc`) VALUES
+(1, 'Avançat'),
+(2, 'Normal'),
+(3, 'Principiant');
+
+-- --------------------------------------------------------
+
+--
 -- Estructura de tabla para la tabla `users`
 --
 
 CREATE TABLE `users` (
-  `id_alias` int(11) NOT NULL,
-  `nombre` varchar(50) NOT NULL,
-  `nivel` int(11) NOT NULL,
-  `zona` varchar(50) NOT NULL
+  `users_id` int(11) NOT NULL,
+  `users_first_name` varchar(50) NOT NULL,
+  `users_last_name` varchar(50) NOT NULL,
+  `users_username` varchar(50) NOT NULL,
+  `users_levels_id` int(5) NOT NULL DEFAULT '0'
 ) ENGINE=InnoDB DEFAULT CHARSET=latin1;
 
 --
 -- Volcado de datos para la tabla `users`
 --
 
-INSERT INTO `users` (`id_alias`, `nombre`, `nivel`, `zona`) VALUES
-(1, 'luciaserer', 1, 'alicante');
+INSERT INTO `users` (`users_id`, `users_first_name`, `users_last_name`, `users_username`, `users_levels_id`) VALUES
+(1, 'luciaserer', '1', 'alicante', 0),
+(136218125, 'esteve', '6', '4', 0);
 
 --
 -- Índices para tablas volcadas
 --
 
 --
+-- Indices de la tabla `levels`
+--
+ALTER TABLE `levels`
+  ADD PRIMARY KEY (`levels_id`);
+
+--
 -- Indices de la tabla `users`
 --
 ALTER TABLE `users`
-  ADD PRIMARY KEY (`id_alias`);
+  ADD PRIMARY KEY (`users_id`);
 COMMIT;
 
 /*!40101 SET CHARACTER_SET_CLIENT=@OLD_CHARACTER_SET_CLIENT */;
